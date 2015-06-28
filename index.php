7d086505ccf003d12a62c85c65b9f167898460d4
Jesus Lizama <dev@hellojeb.us>
2015-06-27 23:14:24 -0700

    finishing touches

--- index.php
+++ index.php
@@ -4,20 +4,22 @@
   <meta charset="UTF-8">
   <title>GSAP Landing</title>
   <link rel="stylesheet" href="/css/style.css"/>
-  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-  <!--CDN links for the latest TweenLite, CSSPlugin, and EasePack-->
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/easing/EasePack.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js"></script>
 </head>
 <body>
 
-<img src="/img/one.png" alt=""/>
-<img src="/img/two.png" alt=""/>
-<img src="/img/three.png" alt=""/>
-<img src="/img/four.png" alt=""/>
-<img src="/img/five.png" alt=""/>
-<img src="/img/six.png" alt=""/>
-
+<div class="images">
+  <img src="/img/one.png" alt=""/>
+  <img src="/img/two.png" alt=""/>
+  <img src="/img/three.png" alt=""/>
+  <img src="/img/four.png" alt=""/>
+  <img src="/img/five.png" alt=""/>
+  <img src="/img/six.png" alt=""/>
+</div>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<!--CDN links for the latest TweenLite, CSSPlugin, and EasePack-->
+<!--<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/easing/EasePack.min.js"></script>-->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+<script src="/js/site.js"></script>
 </body>
 </html>
--- js/site.js
+++ js/site.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
 
-  console.log('youve been rick rolled')
+  console.log('you\'ve been rick rolled');
+
+  var $slides = $('.images img');
+
+  var currentSlide = 0;				//keep track on the current slide
+  var stayTime = 0.7;					//time the slide stays
+  var slideTime = 0.3;				//fade in / fade out time 
+
+  var movement = -30;
+
+  TweenLite.set($slides.filter(":gt(0)"), {autoAlpha:0, y: movement});	//we hide all images after the first one
+  TweenLite.delayedCall(stayTime, nextSlide);				//start the slideshow
+
+  function nextSlide(){
+    if(currentSlide < 5){
+      TweenLite.to( $slides.eq(currentSlide), slideTime, {autoAlpha:0, y: movement} );		//fade out the old slide
+      currentSlide = ++currentSlide % $slides.length;							//find out the next slide
+      TweenLite.to( $slides.eq(currentSlide), slideTime, {autoAlpha:1, y: 0} );		//fade in the next slide
+      TweenLite.delayedCall(stayTime, nextSlide);								//wait a couple of seconds before next slide
+    }
+
+  }
+
 
 });
