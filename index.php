7488b218946561569d9b03c66119e02b7625febe
Jesus Lizama <dev@hellojeb.us>
2015-06-27 21:41:05 -0700

    adding base files

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <title>GSAP</title>
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <!--CDN links for the latest TweenLite, CSSPlugin, and EasePack-->
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/easing/EasePack.min.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js"></script>
 </head>
 <body>
 
