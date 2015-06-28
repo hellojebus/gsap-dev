b99433760295e74c2d2d67f63529b031b6e6470e
Jesus Lizama <dev@hellojeb.us>
2015-06-27 21:53:45 -0700

    more bootstrapping

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>GSAP</title>
+  <title>GSAP Landing</title>
+  <link rel="stylesheet" href="/css/style.css"/>
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
   <!--CDN links for the latest TweenLite, CSSPlugin, and EasePack-->
   <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
@@ -10,6 +11,13 @@
   <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js"></script>
 </head>
 <body>
+
+<img src="/img/one.png" alt=""/>
+<img src="/img/two.png" alt=""/>
+<img src="/img/three.png" alt=""/>
+<img src="/img/four.png" alt=""/>
+<img src="/img/five.png" alt=""/>
+<img src="/img/six.png" alt=""/>
 
 </body>
 </html>
